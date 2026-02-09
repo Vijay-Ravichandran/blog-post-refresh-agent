3b5200faa937681ac3902635b20fe9168d8d642b
Vijay-Ravichandran <vijayravi.0906@gmail.com>
2026-02-09 17:31:52 +0530

    now blog content analysis and link veryfying is created

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ with open("blog_input.txt", "r", encoding="utf-8") as file:
     blog_text = file.read()
 print (blog_text[:500])
 print ("Blog content loaded successfully")
+
+lines = blog_text.split("\n")
+
+sections = []
+links = []
+
+for line in lines:
+    if line.strip().startswith("#"):
+        sections.append(line.strip())
+    if "http" in line:
+        links.append(line.strip())
+
+print(f"Total sections found: {len(sections)}")
+print(f"Total links found: {len(links)}")
